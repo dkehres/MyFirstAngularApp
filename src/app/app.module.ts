1420cb8e01d05806e17c43ccc6a1ff455891d0a2
Dylan Kehres <dkehres@mail.bradley.edu>
2018-12-22 15:34:48 -0500

    Fixed jquery typing error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import * as $ from 'jquery';
 import {DataService} from './data.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { FormsModule } from '@angular/forms'
