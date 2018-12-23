9df00ed306ce7ea4c459fd932a7ae48f497cda3b
Dylan Kehres <dkehres@mail.bradley.edu>
2018-12-23 16:07:52 -0500

    show/hide on radio button click

--- src/app/tourney-info/host-tourney/host-tourney.component.ts
+++ src/app/tourney-info/host-tourney/host-tourney.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../data.service';
-import {Router, ActivatedRoute} from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { IfStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-host-tourney',
@@ -11,7 +12,47 @@ export class HostTourneyComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private router: Router, private _data: DataService) { }
 
+  showPercent: boolean;
+  showFlat: boolean;
+
   ngOnInit() {
+    $(document).ready(function () {
+      // // $('input[type="radio"]').click(function(){
+      //   $(document).on('click','input[type="radio"]', function(event){
+      //   var inputValue = $(this).attr("value");
+      //   var targetDiv = $("."+inputValue);
+      //   $(".optField").not(targetDiv).hide();
+      //   $(targetDiv).show();
+      // });
+
+
+      $('div').on('show',function (e){
+        $('input').removeAttr('checked').removeProp('checked');
+        $(this).find('input').attr('checked','checked').prop('checked','checked');
+      });
+      // $(document).on('click', 'label[id="flatLabel"]', function (event) {
+      //   document.getElementById('percentOpts').style.display = 'none';
+      //   document.getElementById('flatOpts').style.display = '';
+      // });
+    });
   }
 
+  showPayoutDetails(opt: string) {
+    switch (opt) {
+      case 'percent': {
+        this.showPercent = true;
+        this.showFlat = false;
+        break;
+      }
+      case 'flat': {
+        this.showPercent = false;
+        this.showFlat = true;
+        break;
+      }
+
+      default:
+
+        break;
+    }
+  }
 }
